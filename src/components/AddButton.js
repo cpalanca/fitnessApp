d928b4a0ab5c8fc4d1e7ec7079bce0982a7eed02
Carlos <carlospalanca@gmail.com>
2019-06-29 20:49:52 +0200

    Configurado ReactRouteDom

---
+++ src/components/AddButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/AddButton.css'
+import buttomImg from '../images/add.png'
+import { Link } from 'react-router-dom'
+
+function AddButton2(props) {
+   return(
+      <a href="/exercise/new">
+         <img src={buttomImg} className="Fitness-Add" alt="exercise"/>
+      </a>
+   )
+}
+
+const AddButton = () => (
+   <Link to="/exercise/new">
+       <img src={buttomImg} className="Fitness-Add" alt="exercise"/>
+   </Link>
+)
+
+export default AddButton
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 //importar las librerias lo primero
 import React from 'react'
-import exerciseImg from '../images/exercise.png'
 import circlesImg from '../images/circles.png'
 import './styles/Card.css'
 
@@ -8,22 +7,6 @@ import './styles/Card.css'
 // vamos ahora hacer el componente dinamico para poder reutilizarlo cuando queramos
 //con la info que queramos.
 class Card extends React.Component{
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props){
-        super(props)
-        this.state = {
-            image: 'https://www.fitnessenfemenino.com/wp-content/uploads/4_Fondo-de-triceps_cuadricula.gif'
-        }
-    }
-
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({
-                image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
-            })
-        }, 5000);
-    }
-
     render(){
      const { title, img, description, leftColor, rightColor } = this.props
      return (
@@ -36,7 +19,7 @@ class Card extends React.Component{
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={this.state.image} className="float-right" alt="imagen flexi&oacute;n"/>
+                            <img src={img} className="float-right" alt="imagen flexi&oacute;n"/>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
