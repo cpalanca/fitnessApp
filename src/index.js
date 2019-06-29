442e217cff9789d733218d45feb068b4e59e635d
Carlos <carlospalanca@gmail.com>
2019-06-29 18:40:07 +0200

    Video antes de crear mas paginas

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@
 //importamos las librerias
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Welcome from './components/Welcome'
 import Card from './components/Card'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 //ReactDOM.render(__QUE__, __DONDE__)
-ReactDOM.render(<Card 
-                    title="Mi TITULO"
-                    description="Learn amazing streeet workout and calisthenics"
-                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-                    leftColor="#A74CF2"
-                    rightColor="#617BFB"
-                />, container)
+ReactDOM.render(<div>
+                    <Welcome
+                        username="Carlos"
+                    />
+                    <Card 
+                        title="Workout exercise"
+                        description="Learn amazing streeet workout and calisthenics"
+                        img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                        leftColor="#A74CF2"
+                        rightColor="#617BFB"
+                    />
+                </div>, container)
---
+++ src/pages/Exercises.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Welcome from '../components/Welcome'
+import Card from '../components/Card'
+
+class Exercises extends React.Component{
+
+}
+
+export default Exercises
